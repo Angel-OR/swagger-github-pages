062249104432448dbd8514cc3e13b70d18034fb4
Angel Ortiz <angelortiz0398@gmail.com>
2025-04-23 23:47:04 -0600

    FIX : swagger-initializer

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   window.ui = SwaggerUIBundle({
     urls: [
-      { url: "dist/swagger/Process/swagger.yaml", name: "Process" },
-      { url: "dist/swagger/Catalog/swagger.yaml", name: "Catalog" },
-      { url: "dist/swagger/Security/swagger.yaml", name: "Security" }
+      { url: "/dist/swagger/Process/swagger.yaml", name: "Process" },
+      { url: "/dist/swagger/Catalog/swagger.yaml", name: "Catalog" },
+      { url: "/dist/swagger/Security/swagger.yaml", name: "Security" }
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
