88572aa2498de97e46c55bde5450fdeed9ad6838
angelortiz0398 <54503142+angelortiz0398@users.noreply.github.com>
2025-06-10 10:17:26 +0000

    Update swagger-ui to v5.24.1

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,10 +1,9 @@
 window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    urls: [
-      { url: "swagger-process.yaml", name: "Process" },
-      { url: "swagger-catalog.yaml", name: "Catalog" },
-      { url: "swagger-security.yaml", name: "Security" }
-    ],
+    url: "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -16,4 +15,6 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout"
   });
+
+  //</editor-fold>
 };
